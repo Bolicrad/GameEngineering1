3a0f3a7749a241ddff1d7165b5749a463cecf3d0
Lei Zhao <u1322846@umail.utah.edu>
2023-03-04 05:10:55 -0700

    Created template project for the game engine

---
+++ GameEngine/GameTemplate/Game.cpp
@@ -0,0 +1,19 @@
+#include "Game.h"
+void Game::OnInit() {
+
+}
+
+void Game::OnUpdate(float dt) {
+
+}
+
+void Game::OnDestroy() {
+
+}
+const char* Game::GetGameName() {
+	return "Game Template";
+};
+
+void Game::OnKey(unsigned int i_VKeyID, bool bWentDown) {
+
+};
