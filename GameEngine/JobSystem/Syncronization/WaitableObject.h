e8937d841277736ec0f2de3b7aa09b633a934b8a
Lei Zhao <u1322846@umail.utah.edu>
2023-03-31 03:08:47 -0600

    Solved Platform Bug

--- GameEngine/JobSystem/Syncronization/WaitableObject.h
+++ GameEngine/JobSystem/Syncronization/WaitableObject.h
@@ -1,5 +1,29 @@
 #pragma once
 
-#ifdef _PLATFORM_WINDOWS
-#include "WaitableObject.Win.h"
-#endif // _PLATFORM_WINDOWS
+#include <Windows.h>
+
+namespace Engine
+{
+	class WaitableObject
+	{
+	public:
+		typedef DWORD wait_t;
+
+		static const wait_t			WaitInfinite = INFINITE;
+
+		WaitableObject() :
+			m_Handle(INVALID_HANDLE_VALUE)
+		{}
+		virtual ~WaitableObject() {}
+
+		HANDLE GetHandle(void) { return m_Handle; }
+
+	protected:
+		HANDLE					m_Handle;
+
+	private:
+		WaitableObject(const WaitableObject& i_other) = delete;
+		WaitableObject& operator=(const WaitableObject& i_other) = delete;
+	};
+
+} // namespace Engine
